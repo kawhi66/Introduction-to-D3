b32edfdbfe0d916c7646dfb556f77377a73b7e93
kawhi <kawhi_site@163.com>
2019-08-02 18:53:56 +0800

    update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,15 @@ import data from "./census.json";
 import * as d3 from "d3";
 
 console.log(data);
+const width: number = 600;
+const height: number = 400;
+const margin: { top: number; right: number; bottom: number; left: number } = {
+  top: 50,
+  right: 50,
+  bottom: 50,
+  left: 100
+};
+const container: any = d3
+  .create("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom);
